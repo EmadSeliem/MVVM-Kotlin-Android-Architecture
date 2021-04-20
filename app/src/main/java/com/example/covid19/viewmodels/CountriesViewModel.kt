517aea303fdafedca1576627634f846908481b84
emad <pro.emad@gmail.com>
2021-04-21 00:57:52 +0400

    Initial commit

--- app/src/main/java/com/example/covid19/viewmodels/CountriesViewModel.kt
+++ app/src/main/java/com/example/covid19/viewmodels/CountriesViewModel.kt
@@ -21,12 +21,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CountriesViewModel @Inject constructor(
-    private val mainRepository: MainRepository,
-    @ApplicationContext private val context: Context
+        private val mainRepository: MainRepository,
+        @ApplicationContext private val context: Context
 ) :
-    ViewModel() {
+        ViewModel() {
 
     private val _countryList = MutableLiveData<Resource<CountryStatisticsList>>()
+    val countryStatisticListItem = MutableLiveData<CountryStatisticListItem>()
     val countryList: LiveData<Resource<CountryStatisticsList>> get() = _countryList
 
     fun getCountries(country: String) = viewModelScope.launch(Dispatchers.Main) {
@@ -35,6 +36,7 @@ class CountriesViewModel @Inject constructor(
             mainRepository.getCountries(country).let {
                 if (it.isSuccessful) {
                     _countryList.postValue(Resource.success(it.body()))
+                    countryStatisticListItem.postValue(it.body()?.get(0))
                 } else {
                     _countryList.postValue(Resource.error(it.errorBody().toString(), null))
                 }
