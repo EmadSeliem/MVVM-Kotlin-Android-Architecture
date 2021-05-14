36ad1cb9f40b00fb9ea774d04e25a0424dce674d
emad <pro.emad@gmail.com>
2021-05-14 20:55:42 +0400

    Adding withcontext to swtch excution of network call into IO thread instead of main thread.

--- app/src/main/java/com/example/covid19/repository/MainRepository.kt
+++ app/src/main/java/com/example/covid19/repository/MainRepository.kt
@@ -2,9 +2,12 @@ package com.example.covid19.repository
 
 import com.example.covid19.api.ApiService
 import com.example.covid19.utils.CountryList
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(private val apiService: ApiService) {
-    suspend fun getCountries(country:String) = apiService.getCountries(country)
+
+    suspend fun getCountries(country: String) = withContext(IO) { apiService.getCountries(country) }
     fun getStaticCountryList() = CountryList.getCountryList()
 }
